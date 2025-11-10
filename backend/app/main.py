cb56e80c63b576baccb21ab5834c0d4e997d21fb
Each Chen <info@iach.cc>
2025-11-10 22:18:43 +0800

    chore: add CORS middleware to support frontend and local dev

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.core.config import settings
